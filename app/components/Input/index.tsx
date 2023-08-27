578642e6c5e5b492b6ce5989231feae74d09552a
Andy Chow <hohin523work@gmail.com>
2023-08-26 23:23:43 -0700

    Update Input props

--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -6,15 +6,17 @@ interface InputProp {
   onChange: () => void;
 }
 
-export const Input = (): JSX.Element => {
+export const Input = ({ value, onChange }: InputProp): JSX.Element => {
   return (
     <div className="max-w-screen-sm relative rounded-md shadow-sm text-2xl font-mono">
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
         <span className="text-gray-500">$</span>
       </div>
       <input
-        className="bg-slate-200 text-right text-blue-900 p-3 font-semibold block w-full rounded-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500" placeholder="0.00"
-        id="password"
+        className={`bg-slate-200 text-right text-blue-900 p-3 font-semibold block w-full rounded-md  placeholder:text-gray-400`}
+        placeholder="0.00"
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
